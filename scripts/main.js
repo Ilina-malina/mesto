aa64403885fa646a57af96037218e112b3f8350c
Ilina Usmanova <ilina_orchid@mail.ru>
2022-05-24 02:49:10 +0300

    Первая итерация 5.0

--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,9 @@ const openPopup = document.querySelector(".profile__edit-button");
 const closePopup = popup.querySelector(".popup__close");
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__subscribe');
-
-function togglePopup() {
-    popup.classList.toggle("popup__opened");
-}
-
- popup.addEventListener("click", function (event) {
-    if (event.target === event.currentTarget) {
-        togglePopup();
-    }
-}) 
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_description');
-
-nameInput.value = profileName.textContent;
-jobInput.value = profileDescription.textContent;
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_description');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
@@ -28,9 +14,24 @@ function formSubmitHandler (evt) {
 
     profileName.textContent = nameInputValue;
     profileDescription.textContent = jobInputValue;
-    togglePopup();
+    popup.classList.remove("popup__opened");
 }
 
-openPopup.addEventListener("click", togglePopup);
-closePopup.addEventListener("click", togglePopup);
-formElement.addEventListener('submit', formSubmitHandler); 
+openPopup.addEventListener("click", function () {
+    popup.classList.add("popup__opened");
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
+});
+
+closePopup.addEventListener("click", function () {
+    popup.classList.remove("popup__opened");
+});
+
+popup.addEventListener("click", function (event) {
+    if (event.target === event.currentTarget) {
+        popup.classList.remove("popup__opened");
+        
+    }
+});
+
+formElement.addEventListener('submit', formSubmitHandler);
