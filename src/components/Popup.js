b86ca47db419ce1f7b6661bf8120db201f68ab65
Ilina Usmanova <ilina@Ilinas-MacBook-Pro.local>
2022-09-02 17:00:51 +0300

    fixed last remarks

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,16 +5,17 @@ export class Popup {
         this._popup = document.querySelector(popupSelector);
         this._popupOpened = this._popup.querySelector(".popup_opened");
         this._closeBtn = this._popup.querySelector(".popup__close-button");
+        this._handleSetListener = this._handleSetListener.bind(this);
     }
 
     open() {
         this._popup.classList.add("popup_opened");
-        document.addEventListener("keydown", this._handleSetListener.bind(this));
+        document.addEventListener("keydown", this._handleSetListener);
     }
 
     close() {
         this._popup.classList.remove("popup_opened");
-        document.removeEventListener("keydown", this._handleSetListener.bind(this));
+        document.removeEventListener("keydown", this._handleSetListener);
     }
 
     setEventListeners() {
